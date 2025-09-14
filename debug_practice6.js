6a145232f85b193321c96ca9aef2e1d4aedd79fc
Daniel Henderson <djhindu17@gmail.com>
2025-09-13 23:20:23 -0500

    Today

---
+++ debug_practice6.js
@@ -0,0 +1,8 @@
+import subprocess
+
+def show_head_commit():
+    result = subprocess.run(
+        ["git", "log", "-1", "--pretty=format:%h %ad %s", "--date=short"],
+        capture_output=True, text=True
+    )
+    print("HEAD is at:", result.stdout)
